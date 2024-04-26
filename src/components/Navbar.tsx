87cd128c45725aedf9bb77050783d1955691a925
nanooka <102428722+nanooka@users.noreply.github.com>
2024-04-26 22:44:34 +0400

    nanooka

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,24 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  const handleRefreshClick = () => {
+    window.scrollTo(0, 0);
+    window.location.reload();
+  };
   return (
     <div className="navbar">
-      {/* <h3>{"<Nanuka />"}</h3> */}
-      {/* <div> */}
-      <NavLink to={"/"} style={{ textAlign: "center" }}>
-        About Me
-      </NavLink>
-      <NavLink to={"/projects"}>Projects</NavLink>
-      <NavLink to={"/contact"}>Contact</NavLink>
-      {/* </div> */}
+      <div className="nanooka" onClick={handleRefreshClick}>
+        <b style={{ cursor: "pointer" }}>{"<"}</b>
+        <h3 style={{ color: "#319c89", cursor: "pointer" }}>Nanooka</h3>
+        <b style={{ cursor: "pointer" }}>{"/>"}</b>
+      </div>
+      <div className="navlinks">
+        <NavLink to={"/"} style={{ textAlign: "center" }}>
+          About Me
+        </NavLink>
+        <NavLink to={"/projects"}>Projects</NavLink>
+        <NavLink to={"/contact"}>Contact</NavLink>
+      </div>
     </div>
   );
 }
